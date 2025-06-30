684a78f2c75bba1d982d28d419f3391458271532
G384122023 <238412@st.takushoku-u.ac.jp>
2025-06-30 13:50:22 +0900

    第4 回課題

--- kazuate.js
+++ kazuate.js
@@ -1,56 +1,50 @@
 // 答え
-let kotae = Math.floor(Math.random()*10) + 1;
- console.log('答え（デバッグ用）: ' + kotae);
-
+let kotae = Math.floor(Math.random() * 10) + 1;
+console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
 let kaisu = 1;
-let end = false; // ゲーム終了フラグ
-  
+let end = false;
+
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-hantei();
-hantei();
-hantei();
-
- 
-
+document.getElementById('print').addEventListener('click', hantei);
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
-  // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
-  let tmp = true;
-  
-  console.log(kaisu + '回目の予想 : ' + yoso);
+  if (end) {
+    document.getElementById('result').textContent = 'ゲームはすでに終了しています。';
+    return;
+  }
 
-  
-  
-  // 課題3-1: 正解判定する
-  if(kaisu < 4 && end == false){
-    if(kotae >  yoso){
-      console.log('まちがい．答えはもっと大きいですよ');
-    }
-    else if(kotae < yoso){
-      console.log('まちがい．答えはもっと小さいですよ');
-    }
-    else{
-      console.log('正解です．おめでとう!');
+// 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
+  let yoso = Number(document.getElementById('yosou').value);
+
+
+  document.getElementById('kaisu').textContent = kaisu;
+  document.getElementById('answer').textContent = yoso;
+
+  let result = '';
+
+  if (kaisu < 4 && !end) {
+    if (kotae > yoso) {
+      result = 'まちがい．答えはもっと大きいですよ';
+    } else if (kotae < yoso) {
+      result = 'まちがい．答えはもっと小さいですよ';
+    } else {
+      result = '正解です．おめでとう!';
       end = true;
-      tmp = false;
     }
-  }else{
+  } else {
+    result = '答えは ' + kotae + ' でした．すでにゲームは終わっています';
     end = true;
   }
-
-  if(end && tmp){
-    console.log('答えは' + kotae + 'でした．すでにゲームは終わっています');
-  }
-
-  kaisu++;
- 
+  // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
-
+  document.getElementById('result').textContent = result;
+  kaisu++;
 }
+
+
+
